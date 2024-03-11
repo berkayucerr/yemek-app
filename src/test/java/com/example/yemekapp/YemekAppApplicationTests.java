d9255196de94cabefe835eca4cacbc2de2898fde
berkayucerr <berkayucerr@gmail.com>
2024-03-11 21:36:25 +0300

    initial commit

---
+++ src/test/java/com/example/yemekapp/YemekAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yemekapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemekAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
